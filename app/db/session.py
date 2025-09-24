918dc66abe436c04fd5a40ecd6c78d3c7ead9a93
andmoreduro <safic913@gmail.com>
2025-09-24 17:08:50 -0500

    Updated deprecated import of declarative_base.

--- app/db/session.py
+++ app/db/session.py
@@ -3,8 +3,7 @@ Database session management.
 """
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from core.config import settings
 
